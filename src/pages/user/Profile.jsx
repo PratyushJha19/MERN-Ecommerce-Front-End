a02d4967c86a91633d26c9db1dc1bd1c47c564b1
pratyush-jh <63406558+PratyushJha19@users.noreply.github.com>
2025-07-21 21:03:44 +0530

    Profile Update

--- src/pages/user/Profile.jsx
+++ src/pages/user/Profile.jsx
@@ -1,8 +1,57 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Layout from "../../components/Layout/Layout";
 import UserMenu from "../../components/Layout/UserMenu";
+import { UseAuth } from "../../context/auth";
+import axios from "axios";
+import toast from "react-hot-toast";
 
 const Profile = () => {
+  // Context
+  const [auth, setAuth] = UseAuth();
+
+  // State
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [phone, setPhone] = useState("");
+  const [address, setAddress] = useState("");
+
+  // Get user data
+  useEffect(() => {
+    if (auth?.user) {
+      const { email, name, phone, address } = auth.user;
+      setName(name || "");
+      setPhone(phone || "");
+      setEmail(email || "");
+      setAddress(address || "");
+    }
+  }, [auth?.user]);
+
+  // Form Function - To update details
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.put(
+        `${process.env.REACT_APP_API}/api/v1/auth/profile`,
+        { name, email, password, phone, address }
+      );
+
+      if (data?.error) {
+        toast.error(data?.error);
+      } else {
+        setAuth({ ...auth, user: data?.updatedUser });
+        let ls = localStorage.getItem("auth");
+        ls = JSON.parse(ls);
+        ls.user = data.updatedUser;
+        localStorage.setItem("auth", JSON.stringify(ls));
+        toast.success("Profile updated successfully");
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong");
+    }
+  };
+
   return (
     <Layout title="User Profile">
       <div className="container-fluid p-3 m-3">
@@ -11,7 +60,65 @@ const Profile = () => {
             <UserMenu />
           </div>
           <div className="col-md-9">
-            <h1>Profile</h1>
+            <h1>Update Details</h1>
+            <form onSubmit={handleSubmit}>
+              <div className="mb-3">
+                <input
+                  type="text"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  className="form-control"
+                  id="exampleInputName"
+                  aria-describedby="emailHelp"
+                  placeholder="Name"
+                />
+              </div>
+              <div className="mb-3">
+                <input
+                  type="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  className="form-control"
+                  id="exampleInputEmail"
+                  aria-describedby="emailHelp"
+                  placeholder="Email address"
+                  disabled
+                />
+              </div>
+              <div className="mb-3">
+                <input
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  className="form-control"
+                  id="exampleInputPassword"
+                  placeholder="Password"
+                />
+              </div>
+              <div className="mb-3">
+                <input
+                  type="text"
+                  value={phone}
+                  onChange={(e) => setPhone(e.target.value)}
+                  className="form-control"
+                  id="exampleInputPhone"
+                  placeholder="Phone Number"
+                />
+              </div>
+              <div className="mb-3">
+                <input
+                  type="text"
+                  value={address}
+                  onChange={(e) => setAddress(e.target.value)}
+                  className="form-control"
+                  id="exampleInputAddress"
+                  placeholder="Address"
+                />
+              </div>
+              <button type="submit" className="btn btn-primary">
+                UPDATE
+              </button>
+            </form>
           </div>
         </div>
       </div>
